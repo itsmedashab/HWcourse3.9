ebca85c9c250ea02fa4a4fe9c7dd3346cf3b454e
Dasha <dashabutenko@list.ru>
2022-12-19 21:22:13 +0400

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int n;
+Console.Write($"Введите N: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+PrintNumbers(n, 1);
+Console.Write($"\n");
+
+//---------------------------------------
+void PrintNumbers(int last, int first)
+{
+    if (last <= first)
+    {
+        Console.Write($"{last}");
+    }
+    else
+    {
+        Console.Write($"{last} ");
+        PrintNumbers(last - 1, first);
+    }
+}
